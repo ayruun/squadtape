864564dec1278b5736a0ceba7bb0296a902d6883
Aaron <ayruun@icloud.com>
2019-07-11 11:37:16 +0200

    make grid responsive

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -7,22 +7,34 @@
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 <style>
 .cover-section {
   grid-row: 3 / 4;
-  margin: 25px 0;
+  margin: 25px;
 }
 
 .cover-grid {
   display: grid;
-  grid-template-columns: repeat(4, 300px);
-  grid-template-rows: repeat(auto-fill, 300px);
+  grid-template-columns: repeat(4, minmax(auto, 300px));
+  grid-template-rows: repeat(auto-fill, minmax(auto, 300px));
   grid-auto-flow: row;
   justify-content: center;
   grid-gap: 10px;
 }
+
+@media (max-width: 850px) {
+  .cover-grid {
+    grid-template-columns: repeat(3, minmax(auto, 300px));
+  }
+}
+
+@media (max-width: 600px) {
+  .cover-grid {
+    grid-template-columns: repeat(2, minmax(auto, 300px));
+  }
+}
+
 </style>
